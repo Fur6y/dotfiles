b8a42f0e14b6b8cf8786bd4d6e09d641ba14d31e
Fabian Fetting <mail@fabianfetting.de>
2016-03-11 14:33:49 +0100

    Add docker variables

--- config/fisherman/conf.d/init.fish
+++ config/fisherman/conf.d/init.fish
@@ -6,48 +6,53 @@ set -g fish_function_path $fisher_config/my-functions $fish_function_path
 
 ##### EXPORTS #####
 # dotfiles directory
-set -xg DOTFILES $HOME/dotfiles
+set -gx DOTFILES $HOME/dotfiles
 # language default
-set -xg LC_ALL en_US.UTF-8
-set -xg LC_CTYPE en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+set -gx LC_CTYPE en_US.UTF-8
 # editor
-set -xg VISUAL vim
-set -xg EDITOR vim
-set -xg GIT_EDITOR vim
+set -gx VISUAL vim
+set -gx EDITOR vim
+set -gx GIT_EDITOR vim
 # atom
-set -xg ATOM_HOME $DOTFILES/atom
+set -gx ATOM_HOME $DOTFILES/atom
 # grep
-set -xg GREP_OPTIONS "--color=auto"
-set -xg GREP_COLOR "1;31" # red
+set -gx GREP_OPTIONS "--color=auto"
+set -gx GREP_COLOR "1;31" # red
 # java
-set -xg JAVA_HOME (/usr/libexec/java_home)
+set -gx JAVA_HOME (/usr/libexec/java_home)
 # android
-set -xg ANDROID_HOME $HOME/Library/Android/sdk
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
 # ant
-set -xg ANT_HOME /usr/share/ant
+set -gx ANT_HOME /usr/share/ant
 # gradle
-set -xg GRADLE_HOME /opt/homebrew-cask/Caskroom/android-studio/1.4.0.10/Android\ Studio.app/Contents/gradle/gradle-2.8
+set -gx GRADLE_HOME /opt/homebrew-cask/Caskroom/android-studio/1.4.0.10/Android\ Studio.app/Contents/gradle/gradle-2.8
 # maven
-set -xg MAVEN_OPTS="-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
+set -gx MAVEN_OPTS="-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
 # jboss
-set -xg JBOSS_HOME /usr/local/opt/jboss-as/libexec
+set -gx JBOSS_HOME /usr/local/opt/jboss-as/libexec
 # graphiz
-set -xg GRAPHVIZ_DOT /usr/local/Cellar/graphviz/2.38.0/bin/dot
+set -gx GRAPHVIZ_DOT /usr/local/Cellar/graphviz/2.38.0/bin/dot
 # fisherman plugin 'bd'
 set -gx BD_OPT 'insensitive'
+# docker
+set -gx DOCKER_TLS_VERIFY "1"
+set -gx DOCKER_HOST "tcp://192.168.99.100:2376"
+set -gx DOCKER_CERT_PATH $HOME/.docker/machine/machines/default
+set -gx DOCKER_MACHINE_NAME "default"
 
 
 ##### PATH #####
-set -xg PATH $PATH /usr/local/sbin
+set -gx PATH $PATH /usr/local/sbin
 # android
-set -xg PATH $PATH $ANDROID_HOME/platform-tools
-set -xg PATH $PATH $ANDROID_HOME/tools
+set -gx PATH $PATH $ANDROID_HOME/platform-tools
+set -gx PATH $PATH $ANDROID_HOME/tools
 # ant
-set -xg PATH $PATH $ANT_HOME/bin
+set -gx PATH $PATH $ANT_HOME/bin
 # gradle
-set -xg PATH $PATH $GRADLE_HOME/bin
+set -gx PATH $PATH $GRADLE_HOME/bin
 # jboss
-set -xg PATH $PATH $JBOSS_HOME/bin
+set -gx PATH $PATH $JBOSS_HOME/bin
 
 
 ##### ALIASES #####
@@ -65,7 +70,3 @@ alias fishshell="chsh -s /usr/local/bin/fish"
 # osx
 alias hidefiles="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
 alias showfiles="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
-
-
-##### PRIVATE #####
-source $fisher_config/config.private.fish
