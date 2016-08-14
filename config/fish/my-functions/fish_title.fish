c51eb3c6e2d4e1826797f8ca5c25d50768926d56
Fabian Fetting <mail@fabianfetting.de>
2016-08-14 21:04:22 +0200

    Print full path at window title

--- config/fish/my-functions/fish_title.fish
+++ config/fish/my-functions/fish_title.fish
@@ -2,6 +2,15 @@ function fish_title
     if test $session_title
         echo $session_title
     else
-        echo (prompt_pwd)
+        set -l MAX_TITLE_LENGTH 15
+        set -l current_pwd (pwd)
+        set -l current_pwd_length (string length $current_pwd)
+        if test $current_pwd_length -ge $MAX_TITLE_LENGTH
+            set -l start (math $current_pwd_length - $MAX_TITLE_LENGTH)
+            set -l shortened_title (string sub -s $start -l $current_pwd_length $current_pwd)
+            echo "…$shortened_title"
+        else
+            echo (prompt_pwd)
+        end
     end
 end
