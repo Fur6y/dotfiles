39f9accae3662759d8e9310adbb974f94c2a3381
Fabian Fetting <fabian.fetting@greenpocket.de>
2016-08-15 08:57:16 +0200

    Set title on qivicon emulator start

--- config/fish/my-functions/qivicon_emulator.fish
+++ config/fish/my-functions/qivicon_emulator.fish
@@ -1,4 +1,7 @@
 function qivicon_emulator
     cd ~/workspace/target_platforms/Emulator/osgi/bin/vms/jdk;
+    if type --quiet "change_title"
+        change_title "Qivicon Emulator"
+    end
     eval "./server";
 end
